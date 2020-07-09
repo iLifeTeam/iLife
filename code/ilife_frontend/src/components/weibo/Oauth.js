091326c6516c8aee183c2112101f2abba6e240ae
YuyangHuang <50946857+YuyangHuang@users.noreply.github.com>
2020-07-10 00:09:45 +0800

    Revert "feat: alipay Oauth2"
    This reverts commit 3803fecfd305422750abd6b46f79744061864a18.

--- code/ilife_frontend/src/components/weibo/Oauth.js
+++ code/ilife_frontend/src/components/weibo/Oauth.js
@@ -12,7 +12,7 @@ export default class Oauth extends Component {
   oauthLink() {
     console.log("isdkugfsd");
     const _this = this;    //先存一下this，以防使用箭头函数this会指向我们不希望它所指向的对象。
-    axios.post('http://49.234.125.131:80/weibo/wiki/Oauth2/authorize',
+    axios.post('https://open.weibo.com/wiki/Oauth2/authorize',
       {
         client_id: '2181274558',
         redirect_uri: 'https://api.weibo.com/oauth2/default.html',
