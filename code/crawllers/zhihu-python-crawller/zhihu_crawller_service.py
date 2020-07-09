5dadfa465db5bd79a45bdeec9537c9472d8e0feb
赵旭阳 <771906409@qq.com>
2020-07-09 18:29:50 +0800

    feat: finish python part of rpc call, serialize data into json, may add more data and refactor later

--- code/crawllers/zhihu-python-crawller/zhihu_crawller_service.py
+++ code/crawllers/zhihu-python-crawller/zhihu_crawller_service.py
@@ -18,41 +18,100 @@ class MyEncoder(json.JSONEncoder):
 
 
 class Question:
-    title = ""
-    create_time = 0
-    upate_time = 0
-    excerp = ""
-    content = ""
-    answer_count = 0
-    answers = []
+    def __init__(self, title,excerpt,content, create_time, update_time,answer_count):
+        self.title = title
+        self.create_time = create_time
+        self.update_time = update_time
+        self.excerp =excerpt
+        self.content=content
+        self.answer_count = answer_count
+        self.answers = []
+    def __init__(self):
+        self.title = ""
+        self.create_time = 0
+        self.update_time = 0
+        self.excerp =""
+        self.content=""
+        self.answer_count = 0
+        self.answers = []
+
+    def setValue(self,question):
+        self.title = question.title
+        self.content = question.detail
+        self.excerpt = question.excerpt
+        self.create_time = question.created_time
+        self.update_time = question.updated_time
+        self.answer_count = question.answer_count
 
 class Answer:
-    author = ""
-    create_time = 0
-    udpate_time = 0
-    content = ""
-    excerpt = ""
-    voteup_count = 0
-    comment_count = 0
-    question = Question()
+    def __init__(self, author,excerp,content, create_time, update_time,voteup_count,comment_count):
+        self.author = author
+        self.create_time = create_time
+        self.update_time = update_time
+        self.excerpt =excerpt
+        self.content=content
+        self.voteup_count = voteup_count
+        self.comment_count = comment_count
+        self.question = Question()
+    def __init__(self):
+        self.author = ""
+        self.create_time = 0
+        self.update_time = 0
+        self.excerpt =""
+        self.content=""
+        self.voteup_count = 0
+        self.comment_count = 0
+        self.question = Question()
+    def setValue(self,answer):
+        self.author = answer.author.name
+        self.content = answer.content
+        self.excerpt = answer.excerpt
+        self.create_time = answer.created_time
+        self.update_time = answer.updated_time
+        self.voteup_count = answer.voteup_count
+        self.comment_count = answer.comment_count
 
 class Article:
-    title =""
-    author = ""
-    create_time = 0
-    udpate_time = 0
-    excerp = ""
-    content = ""
-    image_url = ""
-    column_name = ""
+    def __init__(self,title, author,excerp,content, create_time, update_time,image_url,column_name):
+        self.title =title
+        self.author = author
+        self.create_time = create_time
+        self.update_time = update_time
+        self.excerpt =excerpt
+        self.content=content
+        self.image_url = image_url
+        self.column_name = column_name
+    def __init__(self):
+        self.title =""
+        self.author = ""
+        self.create_time = 0
+        self.update_time = 0
+        self.excerpt =""
+        self.content=""
+        self.image_url = ""
+        self.column_name = ""
+    def setValue(self,article):
+        self.title = article.title
+        self.author = article.author.name
+        self.content = article.content
+        self.excerpt = article.excerpt
+        self.create_time = article.created_time
+        self.update_time = article.updated_time
+        self.image_url = article.image_url
+        self.column_name = article.column_name
 
 class Activity:
-    type = ""
-    action_text = ""
-    create_time = 0
-    article = Article()
-    answer = Answer()
-    question = Question()
+    def __init__(self,type,action_text,create_time):
+        self.type = type
+        self.action_text = action_text
+        self.create_time =create_time
+        if type == ActType.CREATE_ANSWER or type == ActType.VOTEUP_ANSWER:
+            self.answer = Answer()
+        if type == ActType.CREATE_QUESTION or type == ActType.FOLLOW_QUESTION:
+            self.question = Question()
+        if type == ActType.CREATE_ARTICLE or type == ActType.VOTEUP_ARTICLE:
+            self.article = Article()
+    
 
 class ZhihuService(zhihu_pb2_grpc.ZhihuServiceServicer):
     clientpool = {}
@@ -99,33 +158,47 @@ class ZhihuService(zhihu_pb2_grpc.ZhihuServiceServicer):
                 return zhihu_pb2.LoginResponse(response = err)
 
     def GenActivityJson(self,me):
-        frozen = jsonpickle.encode(self.GetActivityDict(me))
-        return frozen
-        # return json.dumps(self.GetActivityDict(me),cls=MyEncoder) 
+        return json.dumps(self.GetActivityDict(me),default=lambda o: o.__dict__, sort_keys=True, indent=4) 
     def GetActivityDict(self,me):
         result = []
         for act in  me.activities:
-            activity = Activity()
-            activity.action_text = act.action_text
-            activity.type = act.type 
-            activity.create_time = act.created_time
+            activity = Activity(act.type,act.action_text,act.created_time)
 
             if activity.type == ActType.CREATE_ANSWER or activity.type == ActType.VOTEUP_ANSWER:
-                activity.answer.author = act.target.author.name
-                activity.answer.content = act.target.content
-                activity.answer.excerpt = act.target.excerpt
-                activity.answer.create_time = act.target.created_time
-                activity.answer.udpate_time = act.target.updated_time
-                activity.answer.voteup_count = act.target.voteup_count
-                activity.answer.comment_count = act.target.comment_count
-                question = act.target.question
-                activity.question.title = question.title
-                activity.question.answer_count = question.answer_count
-                activity.question.content = question.detail
-                activity.question.excerp = question.excerpt
-                activity.question.create_time = question.created_time
-                activity.question.upate_time = question.updated_time
-            
+                activity.answer.setValue(act.target)
+                # activity.answer.author = act.target.author.name
+                # activity.answer.content = act.target.content
+                # activity.answer.excerpt = act.target.excerpt
+                # activity.answer.create_time = act.target.created_time
+                # activity.answer.udpate_time = act.target.updated_time
+                # activity.answer.voteup_count = act.target.voteup_count
+                # activity.answer.comment_count = act.target.comment_count
+                activity.answer.question.setValue(act.target.question)
+                # question = act.target.question
+                # activity.answer.question.title = question.title
+                # activity.answer.question.answer_count = question.answer_count
+                # activity.answer.question.content = question.detail
+                # activity.answer.question.excerp = question.excerpt
+                # activity.answer.question.create_time = question.created_time
+                # activity.answer.question.upate_time = question.updated_time
+            if activity.type == ActType.CREATE_QUESTION or activity.type == ActType.FOLLOW_QUESTION:
+                activity.question.setValue(act.target)
+                # activity.question.title = act.target.title
+                # activity.question.content = act.question.content
+                # activity.question.excerpt = act.question.excerpt
+                # activity.question.create_time = act.question.created_time
+                # activity.question.udpate_time = act.question.updated_time
+                # activity.question.answer_count = act.target.answer_count
+                counter = 10
+                for answer in act.target.answers:
+                    ans = Answer()
+                    ans.setValue(answer)
+                    activity.question.answers.append(ans)
+                    counter = counter - 1
+                    if counter == 0:
+                        break
+            if activity.type == ActType.CREATE_ARTICLE or activity.type == ActType.VOTEUP_ARTICLE:
+                activity.article.setValue(act.target)
             result.append(activity)
             if len(result) >= 10:
                 break
