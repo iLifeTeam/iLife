ae4f14667e3ff57e4585f9c2158162c087a1ff0d
qmyyxtl <1410661066@qq.com>
2020-07-10 09:49:05 +0800

    update：整合知乎数据库
    整合知乎数据库，ilife的users新增zhid属性

--- sql/iLifeinit.sql
+++ sql/iLifeinit.sql
@@ -2,6 +2,72 @@ drop database if exists iLife;
 drop database if exists wyy;
 drop database if exists weibo;
 
+
+
+
+
+
+/* zhihu */
+drop database if exists `zhihu`;
+create database `zhihu`;
+use `zhihu`;
+DROP TABLE IF EXISTS `zhihu`.`answer`;
+DROP TABLE IF EXISTS `zhihu`.`activity`;
+DROP TABLE IF EXISTS `zhihu`.`question`;
+DROP TABLE IF EXISTS `zhihu`.`article`;
+
+CREATE TABLE `zhihu`.`activity` (
+  `activity_id` INT NOT NULL AUTO_INCREMENT,
+  `zhihu_user_id` VARCHAR(30) NOT NULL,
+  `type` VARCHAR(20) NOT NULL,
+  `action_text` VARCHAR(20) NULL,
+  `target_id` INT NULL,
+  `create_time` TIMESTAMP NULL,
+  PRIMARY KEY (`activity_id`)
+  )character set utf8mb4;
+  
+-- activity type : 
+
+CREATE TABLE `zhihu`.`question` (
+  `question_id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(50) NULL,
+  `create_time` TIMESTAMP NULL,
+  `update_time` TIMESTAMP NULL,
+  `excerpt` VARCHAR(300) NULL,
+  `content` MEDIUMTEXT NULL,
+  `answer_count` INT NULL,
+  PRIMARY KEY (`question_id`)
+  )character set utf8mb4;
+  
+CREATE TABLE `zhihu`.`answer` (
+  `answer_id` INT NOT NULL AUTO_INCREMENT,
+  `question_id` INT NOT NULL,
+  `author` VARCHAR(45) NULL,
+  `content` MEDIUMTEXT NULL,
+  `create_time` TIMESTAMP NULL,
+  `update_time` TIMESTAMP NULL,
+  `excerpt` VARCHAR(300) NULL,
+  `voteup_count` INT NULL,
+  `comment_count` INT NULL,
+  PRIMARY KEY (`answer_id`),
+  FOREIGN KEY (`question_id`) references `zhihu`.`question`(`question_id`)
+  )character set utf8mb4;
+CREATE TABLE `zhihu`.`article` (
+  `article_id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NULL,
+  `author` VARCHAR(45) NULL,
+  `excerpt` VARCHAR(300) NULL,
+  `content` MEDIUMTEXT NULL,
+  `image_url` VARCHAR(200) NULL,
+  `column_name` VARCHAR(45) NULL,
+  `update_time` TIMESTAMP NULL,
+  PRIMARY KEY (`article_id`)
+  )character set utf8mb4;
+
+
+
+
+
 /* wyy */
 CREATE database wyy;
 USE wyy;
@@ -107,6 +173,7 @@ create table users
    id                   bigint not null,
    wyyid                bigint,
    weibid               bigint,
+   zhid                 varchar(30),
    nickname             varchar(20),
    account              char(20),
    password             char(20),
