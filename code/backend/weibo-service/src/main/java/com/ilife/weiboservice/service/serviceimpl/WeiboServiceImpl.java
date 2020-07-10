644ef42e46910ee26ab04f911a1eac433022708c
ygen <zhull991122@sjtu.edu.cn>
2020-07-10 21:15:25 +0800

    lint:use linter to format the file

--- code/backend/weibo-service/src/main/java/com/ilife/weiboservice/service/serviceimpl/WeiboServiceImpl.java
+++ code/backend/weibo-service/src/main/java/com/ilife/weiboservice/service/serviceimpl/WeiboServiceImpl.java
@@ -16,15 +16,15 @@ public class WeiboServiceImpl implements WeiboService {
     @Autowired
     private WeiboDao weiboDao;
 
-    public List<Weibo> findAllByUid(Integer uid){
+    public List<Weibo> findAllByUid(Integer uid) {
         return weiboDao.findAllByUid(uid);
     }
 
-    public void crawlWeibo(Integer uid){
+    public void crawlWeibo(Integer uid) {
         try {
             // TODO:should go to crawl.py and modify some parameter
-            String[] args=new String[]{"python","crawler\\weiboSpider\\weibo_spider\\crawl.py",uid.toString()};
-            Process pr =Runtime.getRuntime().exec(args);
+            String[] args = new String[]{"python", "crawler\\weiboSpider\\weibo_spider\\crawl.py", uid.toString()};
+            Process pr = Runtime.getRuntime().exec(args);
             //Runtime.exec 方法创建一个本机进程，并返回 Process 子类的一个实例，该实例可用来控制进程并获取相关信息。
             BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));//建立一个BufferedReader对象，从字符输入流中读取文本即读取python脚本
             String line;
@@ -32,11 +32,11 @@ public class WeiboServiceImpl implements WeiboService {
                 System.out.println(line);
             }
             in.close();
-            int r2=pr.waitFor();//等待进程运行完成再向下执行
-            System.out.println("end"+r2);
-            }catch (Exception e1) {
+            int r2 = pr.waitFor();//等待进程运行完成再向下执行
+            System.out.println("end" + r2);
+        } catch (Exception e1) {
             e1.printStackTrace();
         }
 
-        }
+    }
 }
