83655142179031321e650775c756387086fa7001
赵旭阳 <771906409@qq.com>
2020-07-12 23:22:37 +0800

    fix: fix dependency bug

--- code/backend/zhihu-service/src/main/java/com/ilife/zhihu/controller/ZhihuController.java
+++ code/backend/zhihu-service/src/main/java/com/ilife/zhihu/controller/ZhihuController.java
@@ -9,7 +9,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
-import org.graalvm.compiler.lir.LIRInstruction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
