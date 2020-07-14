b8cec830e190247b09cc5e004cfc8d6938a47da5
ygen <zhull991122@sjtu.edu.cn>
2020-07-14 16:53:31 +0800

    fix:modify gitignore and pom.xml

--- code/backend/weibo-service/src/main/java/com/ilife/weiboservice/service/serviceimpl/UserServiceImpl.java
+++ code/backend/weibo-service/src/main/java/com/ilife/weiboservice/service/serviceimpl/UserServiceImpl.java
@@ -1,16 +1,11 @@
 package com.ilife.weiboservice.service.serviceimpl;
 
 import com.ilife.weiboservice.dao.UserDao;
-import com.ilife.weiboservice.dao.WeiboDao;
 import com.ilife.weiboservice.entity.User;
-import com.ilife.weiboservice.entity.Weibo;
 import com.ilife.weiboservice.service.UserService;
-import com.ilife.weiboservice.service.WeiboService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
