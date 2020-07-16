5b65fa2422d0b6578eb41d56d844d15c85d17bb5
ygen <zhull991122@sjtu.edu.cn>
2020-07-16 13:55:52 +0800

    feat:add junit4 support

---
+++ code/backend/auth-service/src/test/java/com/ilife/authservice/controller/UserServiceControllerTest.java
@@ -0,0 +1,115 @@
+package com.ilife.authservice.controller; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* UserServiceController Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Jul 16, 2020</pre> 
+* @version 1.0 
+*/ 
+public class UserServiceControllerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getUserById(@ApiParam(name = "userId", value = "The user ID of a iLife user") @RequestParam("userId") Long uid) 
+* 
+*/ 
+@Test
+public void testGetUserById() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getUserByAccount(@ApiParam(name = "account", value = "The account number of a iLife user") @RequestParam("account") String account) 
+* 
+*/ 
+@Test
+public void testGetUserByAccount() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getUserByNickname(@ApiParam(name = "nickname", value = "The user nickname of a iLife user") @RequestParam("nickname") String nickname) 
+* 
+*/ 
+@Test
+public void testGetUserByNickname() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: deleteById(@ApiParam(name = "userId", value = "The user ID of a iLife user") @RequestBody Map<String, String> params) 
+* 
+*/ 
+@Test
+public void testDeleteById() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: register(@ApiIgnore @RequestBody Map<String, String> params) 
+* 
+*/ 
+@Test
+public void testRegister() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: auth(@ApiIgnore @RequestBody Map<String, String> params) 
+* 
+*/ 
+@Test
+public void testAuth() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: updateWyy(@ApiIgnore @RequestBody Map<String, String> params) 
+* 
+*/ 
+@Test
+public void testUpdateWyy() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: updateWb(@ApiIgnore @RequestBody Map<String, String> params) 
+* 
+*/ 
+@Test
+public void testUpdateWb() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: updateZh(@ApiIgnore @RequestBody Map<String, String> params) 
+* 
+*/ 
+@Test
+public void testUpdateZh() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
