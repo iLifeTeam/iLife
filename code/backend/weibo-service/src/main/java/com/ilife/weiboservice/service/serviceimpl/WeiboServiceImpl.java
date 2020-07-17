1c0b19a525f798032c34c04b16a49acf926f7156
ygen <zhull991122@sjtu.edu.cn>
2020-07-18 00:14:11 +0800

    unfix:some tricky bug unfix

--- code/backend/weibo-service/src/main/java/com/ilife/weiboservice/service/serviceimpl/WeiboServiceImpl.java
+++ code/backend/weibo-service/src/main/java/com/ilife/weiboservice/service/serviceimpl/WeiboServiceImpl.java
@@ -48,21 +48,23 @@ public class WeiboServiceImpl implements WeiboService {
     public void crawlWeibo(Long uid) {
         try {
             // TODO:should go to crawl.py and modify some parameter
-            String[] args = new String[]{"python", "crawler\\weiboSpider\\weibo_spider\\crawl.py", uid.toString()};
+            String[] args = new String[]{"python3", "crawler\\weiboSpider\\weibo_spider\\crawl.py", uid.toString()};
+            System.out.println("start crawling");
             Process pr = Runtime.getRuntime().exec(args);
             //Runtime.exec 方法创建一个本机进程，并返回 Process 子类的一个实例，该实例可用来控制进程并获取相关信息。
             BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream(), StandardCharsets.UTF_8));//建立一个BufferedReader对象，从字符输入流中读取文本即读取python脚本
             String line;
             SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");//设置日期格式
             String date = df.format(new Date());
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("log/crawler_log/" + date + ".log"), StandardCharsets.UTF_8));
+            //BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("log\\crawler_log\\" + date + ".log"), StandardCharsets.UTF_8));
             while ((line = in.readLine()) != null) {
-                out.write(line);
-                out.write("\n");
+//                out.write(line);
+//                out.write("\n");
+                System.out.println(line);
             }
             in.close();
-            out.flush();
-            out.close();
+//            out.flush();
+//            out.close();
             int r2 = pr.waitFor();//等待进程运行完成再向下执行
             System.out.println("end" + r2);
         } catch (Exception e1) {
