16076ac0249fa4f55c402206511c1914e5af2ae0
ygen <zhull991122@sjtu.edu.cn>
2020-07-19 18:54:49 +0800

    feat:finish exposing weibo-crawler interface

--- code/crawllers/weibo-python-crawller/weibo_spider/crawl.py
+++ code/crawllers/weibo-python-crawller/weibo_spider/crawl.py
@@ -4,26 +4,26 @@ import json
 
 
 class Crawl:
-    def __init__(self):
-        self.id = sys.argv[1]
+    def __init__(self, userId):
+        self.id = userId
 
     def crawl(self):
         # TODO:部署在服务器上时，应该将绝对地址改为服务器上的地址
-        os.chdir('.\\crawler\\weiboSpider')
+        # os.chdir('.\\crawler\\weiboSpider')
         os.system('python -m weibo_spider')
 
     def read_json(self):
-        with open('.\\crawler\\weiboSpider\\config.json', 'r', encoding='utf8')as fp:
+        with open('config.json', 'r', encoding='utf8')as fp:
             json_data = json.load(fp)
             array = [str(self.id)]
             json_data["user_id_list"] = array
-        with open('.\\crawler\\weiboSpider\\config.json', 'w', encoding='utf8')as fp:
+        with open('config.json', 'w', encoding='utf8')as fp:
             fp.write(json.dumps(json_data))
 
 
 if __name__ == '__main__':
     try:
-        crawl = Crawl()
+        crawl = Crawl(sys.argv[1])
         crawl.read_json()
         crawl.crawl()
     except Exception as e:
