7e38249527a34f98790a15e1a536c3ffe4c84234
ygen <zhull991122@sjtu.edu.cn>
2020-07-20 17:12:26 +0800

    fix:modify SecurityConfiguration

--- code/backend/auth-service/src/main/java/com/ilife/authservice/config/SecurityConfiguration.java
+++ code/backend/auth-service/src/main/java/com/ilife/authservice/config/SecurityConfiguration.java
@@ -42,24 +42,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
                 .withUser("admin").password(new BCryptPasswordEncoder().encode("pwd")).roles("USER", "ADMIN");
     }
+    /**
+     * 匹配 "/","login","register" 路径，不需要权限即可访问
+     * 匹配 "/home" 及其以下所有路径，都需要 "USER" 权限
+     * 匹配 "/admin" 及其以下所有路径，都需要 "ADMIN" 权限
+     * 登录地址为 "/login"，登录成功默认跳转到页面 "/home"
+     * 退出登录的地址为 "/logout"，退出成功后跳转到页面 "/login"
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/","login","register").permitAll()
+                .antMatchers("/home/**").hasRole("USER")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/home")
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/login");
+    }
 }
-//    /**
-//     * 匹配 "/","/index" 路径，不需要权限即可访问
-//     * 匹配 "/user" 及其以下所有路径，都需要 "USER" 权限
-//     * 匹配 "/admin" 及其以下所有路径，都需要 "ADMIN" 权限
-//     * 登录地址为 "/login"，登录成功默认跳转到页面 "/user"
-//     * 退出登录的地址为 "/logout"，退出成功后跳转到页面 "/login"
-//     */
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/","/index","/error").permitAll()
-//                .antMatchers("/user/**").hasRole("USER")
-//                .antMatchers("/admin/**").hasRole("ADMIN")
-//                .and()
-//                .formLogin().loginPage("/login").defaultSuccessUrl("/user")
-//                .and()
-//                .logout().logoutUrl("/logout").logoutSuccessUrl("/login");
-//    }
-//}
