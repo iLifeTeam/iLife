b43c908456846e0d5ed7734b8c0775d5de56443e
ygen <zhull991122@sjtu.edu.cn>
2020-07-22 09:56:05 +0800

    feat:add douban back end framework

---
+++ code/backend/douban-service/src/test/java/com/ilife/douban/DoubanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilife.douban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
