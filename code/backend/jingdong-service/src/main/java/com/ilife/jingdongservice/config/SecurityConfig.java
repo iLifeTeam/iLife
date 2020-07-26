09b930032d26e317507663c9bd7f006b14372495
赵旭阳 <771906409@qq.com>
2020-07-27 00:14:56 +0800

    security: add cors

--- code/backend/jingdong-service/src/main/java/com/ilife/jingdongservice/config/SecurityConfig.java
+++ code/backend/jingdong-service/src/main/java/com/ilife/jingdongservice/config/SecurityConfig.java
@@ -11,7 +11,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .cors().and()
+                .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
