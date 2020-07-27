4d15d1b3957944eab1f6f300a55a589c5c784d49
ygen <zhull991122@sjtu.edu.cn>
2020-07-27 14:09:49 +0800

    feat:create static .yaml swagger2 file

--- code/backend/douban-service/src/main/java/com/ilife/douban/controller/DoubanServiceController.java
+++ code/backend/douban-service/src/main/java/com/ilife/douban/controller/DoubanServiceController.java
@@ -15,14 +15,13 @@ import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
-
 @Api(tags = {"Douban Service Controller"}, description = "Everything about auth & CRUD of iLife User")
 public class DoubanServiceController {
     @Autowired
     private UserService userService;
 
     @ApiOperation(notes = "Get user info by userID", value = "get user info by id", httpMethod = "GET")
-    @RequestMapping(path = "/douban/getById")
+    @GetMapping(path = "/douban/getById")
     @PreAuthorize("hasRole('ROLE_USER')")
     public User getUserById(@ApiParam(name = "userId", value = "The user ID of a douban user") @RequestParam("userId") String uid) {
         System.out.println("********** getUserById **********");
@@ -33,11 +32,11 @@ public class DoubanServiceController {
             @ApiResponse(code = 501, message = "user Id not exists"),
     })
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "nickname", value = "the nickname of the iLife user"),
+            @ApiImplicitParam(name = "nickname", value = "the nickname of the douban user"),
     }
     )
     @ApiOperation(notes = "Delete one douban user By user Id", value = "delete one user", httpMethod = "POST")
-    @RequestMapping(path = "/douban/delById")
+    @PostMapping(path = "/douban/delById")
     @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<?> deleteById(@ApiParam(name = "userId", value = "The user ID of a douban user") @RequestBody Map<String, String> params) {
         String id = params.get("userId");
@@ -46,7 +45,7 @@ public class DoubanServiceController {
     }
 
     @ApiOperation(notes = "get douban user's read list By user Id", value = "get user's books", httpMethod = "GET")
-    @RequestMapping(path = "/douban/getBooks")
+    @GetMapping(path = "/douban/getBooks")
     @PreAuthorize("hasRole('ROLE_USER')")
     public List<Book> getBooks(@ApiParam(name = "userId", value = "The user ID of a douban user") @RequestParam("userId") String uid) {
         System.out.println("********** getBooks **********");
@@ -54,10 +53,42 @@ public class DoubanServiceController {
     }
 
     @ApiOperation(notes = "get douban user's watch list By user Id", value = "get user's movies", httpMethod = "GET")
-    @RequestMapping(path = "/douban/getMovies")
+    @GetMapping(path = "/douban/getMovies")
     @PreAuthorize("hasRole('ROLE_USER')")
     public List<Movie> getMovies(@ApiParam(name = "userId", value = "The user ID of a douban user") @RequestParam("userId") String uid) {
         System.out.println("********** getMovies **********");
         return userService.getMoviesById(uid);
+    }
+
+    @ApiResponses({
+            @ApiResponse(code = 501, message = "user Id not exists"),
+    })
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value = "the ID of the douban user"),
+    }
+    )
+    @ApiOperation(notes = "Delete Books By user Id", value = "delete books", httpMethod = "POST")
+    @PostMapping(path = "/douban/delBooks")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<?> deleteBooks(@RequestBody Map<String, String> params) {
+        String id = params.get("userId");
+        System.out.println("********** deleteBooks **********");
+        return userService.deleteBooks(id);
+    }
+
+    @ApiResponses({
+            @ApiResponse(code = 501, message = "user Id not exists"),
+    })
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value = "the ID of the douban user"),
+    }
+    )
+    @ApiOperation(notes = "Delete Books By user Id", value = "delete movies", httpMethod = "POST")
+    @PostMapping(path = "/douban/delMovies")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<?> deleteMovies(@RequestBody Map<String, String> params) {
+        String id = params.get("userId");
+        System.out.println("********** deleteMovies **********");
+        return userService.deleteMovies(id);
     }
 }
