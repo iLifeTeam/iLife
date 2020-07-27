be9ef8adc209721f628bf669177b3438968a5c09
ygen <zhull991122@sjtu.edu.cn>
2020-07-27 08:38:50 +0800

    update:add cors support for weibo and douban

--- code/backend/douban-service/src/main/java/com/ilife/douban/config/SecurityConfiguration.java
+++ code/backend/douban-service/src/main/java/com/ilife/douban/config/SecurityConfiguration.java
@@ -12,7 +12,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.cors().and().authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
