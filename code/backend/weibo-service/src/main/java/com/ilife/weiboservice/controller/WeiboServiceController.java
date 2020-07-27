297c10d8aeb2a60cfa53cb2888dcb37b93502b85
ygen <zhull991122@sjtu.edu.cn>
2020-07-27 15:37:21 +0800

    fix:fix CORS problem

--- code/backend/weibo-service/src/main/java/com/ilife/weiboservice/controller/WeiboServiceController.java
+++ code/backend/weibo-service/src/main/java/com/ilife/weiboservice/controller/WeiboServiceController.java
@@ -8,11 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 
 
-@CrossOrigin(origins = "*")
 @RestController
 @Api(value = "/pet")
 public class WeiboServiceController {
@@ -23,16 +23,18 @@ public class WeiboServiceController {
     @ApiOperation(notes = "Get all Weibos from database of one user specified by userID", value = "get one user's Weibos", httpMethod = "GET")
     @GetMapping(path = "/weibo/getWeibos")
     @PreAuthorize("hasRole('ROLE_USER')")
-    public List<Weibo> getWeibos(@ApiParam(name = "userId", value = "The user ID of a WeiBo user,should be a Long Integer") @RequestParam("userId") Long uid) {
+    public List<Weibo> getWeibos(@ApiParam(name = "userId", value = "The user ID of a WeiBo user,should be a Long Integer") @RequestParam("userId") Long uid, HttpServletResponse response) {
         System.out.println("********** getWeibos **********");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         return weiboService.findAllByUid(uid);
     }
 
     @ApiOperation(notes = "Get One Weibo from database specified by Weibo ID", value = "get one Weibo", httpMethod = "GET")
     @GetMapping(path = "/weibo/getWeibo")
     @PreAuthorize("hasRole('ROLE_USER')")
-    public Weibo getWeibo(@ApiParam(name = "Id", value = "The ID of a WeiBo,should be a String") @RequestParam("Id") String id) {
+    public Weibo getWeibo(@ApiParam(name = "Id", value = "The ID of a WeiBo,should be a String") @RequestParam("Id") String id,HttpServletResponse response) {
         System.out.println("********** getWeibo **********");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         return weiboService.findById(id);
     }
 
@@ -42,8 +44,9 @@ public class WeiboServiceController {
     @ApiOperation(notes = "Delete all Weibos from database of one user specified by userID,success if the response.status = 200 ", value = "delete one user's Weibos", httpMethod = "GET")
     @GetMapping(path = "/weibo/deleteWeibos")
     @PreAuthorize("hasRole('ROLE_USER')")
-    public ResponseEntity<?> deleteWeibos(@ApiParam(name = "userId", value = "The user ID of a WeiBo user,should be a Long Integer") @RequestParam("userId") Long uid) {
+    public ResponseEntity<?> deleteWeibos(@ApiParam(name = "userId", value = "The user ID of a WeiBo user,should be a Long Integer") @RequestParam("userId") Long uid,HttpServletResponse response){
         System.out.println("********** deleteWeibos **********");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         return weiboService.deleteByUid(uid);
     }
 
@@ -53,8 +56,9 @@ public class WeiboServiceController {
     @ApiOperation(notes = "Delete one Weibos from database specified by Weibo ID,success if the response.status = 200 ", value = "delete one Weibo", httpMethod = "GET")
     @GetMapping(path = "/weibo/deleteWeibo")
     @PreAuthorize("hasRole('ROLE_USER')")
-    public ResponseEntity<?> deleteWeibo(@ApiParam(name = "Id", value = "The ID of a WeiBo,should be a String") @RequestParam("Id") String id) {
+    public ResponseEntity<?> deleteWeibo(@ApiParam(name = "Id", value = "The ID of a WeiBo,should be a String") @RequestParam("Id") String id,HttpServletResponse response) {
         System.out.println("********** deleteWeibos **********");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         return weiboService.deleteById(id);
     }
 
