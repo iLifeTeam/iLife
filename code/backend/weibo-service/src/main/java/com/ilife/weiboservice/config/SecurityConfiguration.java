f37da92fe9e75d7fddbc6e404b7d3128e7cd7bfe
ygen <zhull991122@sjtu.edu.cn>
2020-07-30 22:26:07 +0800

    feat:about docker

--- code/backend/weibo-service/src/main/java/com/ilife/weiboservice/config/SecurityConfiguration.java
+++ code/backend/weibo-service/src/main/java/com/ilife/weiboservice/config/SecurityConfiguration.java
@@ -13,6 +13,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().authorizeRequests()
+                .antMatchers("/actuator/**","/**").permitAll()
                 .antMatchers("/weibo/getWeibos","/weibo/getStats").permitAll()
                 .anyRequest().authenticated()
                 .and()
