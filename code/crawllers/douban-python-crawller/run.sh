7f2ff38854e989ce7775540fea2dfef49f47ba33
ygen <zhull991122@sjtu.edu.cn>
2020-07-30 21:22:56 +0800

    feat:change ip

--- code/crawllers/douban-python-crawller/run.sh
+++ code/crawllers/douban-python-crawller/run.sh
@@ -1,4 +1,4 @@
-docker stop douban-service
-docker rm douban-service
-docker build . -t ilife2020/douban-service
-docker run -p 8383:8080 -d --name douban-service ilife2020/douban-service
+docker stop douban-crawler
+docker rm douban-crawler
+docker build . -t ilife2020/douban-crawler
+docker run -p 8484:5000 -d --name douban-crawler ilife2020/douban-crawler
