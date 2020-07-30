3672e9f1cc99d1f0c44e7c9f0050203074e54eae
ygen <zhull991122@sjtu.edu.cn>
2020-07-31 07:05:21 +0800

    fix:security problem

--- code/backend/weibo-service/src/main/java/com/ilife/weiboservice/controller/UserServiceController.java
+++ code/backend/weibo-service/src/main/java/com/ilife/weiboservice/controller/UserServiceController.java
@@ -20,7 +20,7 @@ public class UserServiceController {
 
     @ApiOperation(notes = "Get user info by userID", value = "get user info", httpMethod = "GET")
     @GetMapping(path = "/user/getById")
-    @PreAuthorize("hasRole('ROLE_USER')")
+   // @PreAuthorize("hasRole('ROLE_USER')")
     public User getUserById(@ApiParam(name = "userId", value = "The user ID of a WeiBo user,should be a Long Integer") @RequestParam("userId") Long uid, HttpServletResponse response) {
         System.out.println("********** getUserByUserId **********");
         response.setHeader("Access-Control-Allow-Credentials", "true");
@@ -29,7 +29,7 @@ public class UserServiceController {
 
     @ApiOperation(notes = "Get user info by nickname", value = "get user info", httpMethod = "GET")
     @GetMapping(path = "/user/getByName")
-    @PreAuthorize("hasRole('ROLE_USER')")
+   // @PreAuthorize("hasRole('ROLE_USER')")
     public User getUserByNickname(@ApiParam(name = "nickname", value = "The nickname of a WeiBo user,should be a String") @RequestParam("nickname") String nickname,HttpServletResponse response) {
         System.out.println("********** getUserByNickname **********");
         response.setHeader("Access-Control-Allow-Credentials", "true");
@@ -38,7 +38,7 @@ public class UserServiceController {
 
     @ApiOperation(notes = "Delete one user info specified by userId", value = "delete one user", httpMethod = "GET")
     @GetMapping(path = "/user/delById")
-    @PreAuthorize("hasRole('ROLE_USER')")
+  //  @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<?> delUserByUserId(@ApiParam(name = "userId", value = "The user ID of a WeiBo user,should be a Long Integer") @RequestParam("userId") Long uid,HttpServletResponse response) {
         System.out.println("********** deleteUser **********");
         response.setHeader("Access-Control-Allow-Credentials", "true");
