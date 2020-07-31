946f5d6c44fd8954bd88b13c2c6e8b65dbcf0fa8
ygen <zhull991122@sjtu.edu.cn>
2020-07-31 09:22:09 +0800

    fix:cors fix

--- code/crawllers/douban-python-crawller/rest_interface.py
+++ code/crawllers/douban-python-crawller/rest_interface.py
@@ -1,7 +1,19 @@
 from flask import Flask, request
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": ["http://49.234.125.131", "http://localhost:3000"]}})
+
+
+@app.after_request
+def after_request(response):
+    print(response)
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
 
 
 @app.route('/douban/crawlMovie', methods=['GET'])
--- code/crawllers/douban-python-crawller/run.sh
+++
@@ -1,4 +0,0 @@
-docker stop douban-crawler
-docker rm douban-crawler
-docker build . -t ilife2020/douban-crawler
-docker run -p 8484:5000 -d --name douban-crawler ilife2020/douban-crawler
