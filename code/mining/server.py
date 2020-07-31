5b91a8bbdfb5db5dbb1885c527050b0be8bf3f2e
zhaoxuyang <771906409@qq.com>
2020-07-31 00:42:28 +0000

    deploy: change word-cloud cord

--- code/mining/server.py
+++ code/mining/server.py
@@ -17,6 +17,14 @@ def createDirIfNotExists(path):
         os.makedirs(path)
 
 
+@app.after_request
+def after_request(response):
+  #response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  response.headers.add('Access-Control-Allow-Credentials', 'true')
+  return response
+
 @app.route("/word_cloud", methods=['GET'])
 def word_cloud():
     if not request.args or 'uid' not in request.args:
