cbdb564e61c6f84c0edae1fda40ffd6bb7384ea2
赵旭阳 <771906409@qq.com>
2020-09-05 22:20:51 +0800

    feat: zhihu through proxy

--- code/backend/zhihu-service/run.sh
+++ code/backend/zhihu-service/run.sh
@@ -3,4 +3,4 @@ docker stop zhihu-service && docker rm zhihu-service
 mvn clean package -Dmaven.test.skip=true
 docker build . -t ilife2020/zhihu-service
 docker run -p 8094:8090 --name=zhihu-service --network backend_default -d ilife2020/zhihu-service
-docker run -p 8103:8103 --name=zhihu-crawler --network backend_default -d life2020/zhihu-python-crawler
+docker run -p 8103:8103 --name=zhihu-crawler --network backend_default -d ilife2020/zhihu-python-crawler
