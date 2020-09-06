9a1ae1bfe50843aa71c1e896140d515d4d5220bd
赵旭阳 <771906409@qq.com>
2020-09-06 20:49:45 +0800

    fix: alter crawler config

--- code/backend/taobao-service/src/test/java/com/ilife/taobaoservice/service/TaobaoServiceTest.java
+++ code/backend/taobao-service/src/test/java/com/ilife/taobaoservice/service/TaobaoServiceTest.java
@@ -26,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@Transactional
+//@Transactional
 class TaobaoServiceTest {
 
     @Autowired
