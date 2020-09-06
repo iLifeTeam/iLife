5e7f7a6978d82e9de326d3d9a7d434d8ab091068
sjtu-biggie <zhull991122@sjtu.edu.cn>
2020-09-06 09:24:25 +0800

    feat:eureka added

--- code/backend/weibo-service/src/main/java/com/ilife/weiboservice/config/AjaxCorsFilter.java
+++ code/backend/weibo-service/src/main/java/com/ilife/weiboservice/config/AjaxCorsFilter.java
@@ -12,27 +12,27 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
 import java.util.List;
 
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class AjaxCorsFilter extends CorsFilter {
-    public AjaxCorsFilter() {
-        super(configurationSource());
-    }
-
-    private static UrlBasedCorsConfigurationSource configurationSource() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        List<String> allowedHeaders = Arrays.asList("*");
-        List<String> exposedHeaders = Arrays.asList(HttpHeaders.ACCEPT,HttpHeaders.AUTHORIZATION,HttpHeaders.COOKIE,HttpHeaders.ORIGIN,HttpHeaders.HOST);
-        List<String> allowedMethods = Arrays.asList("POST", "GET", "DELETE", "PUT", "OPTIONS");
-        List<String> allowedOrigins = Arrays.asList("http://49.234.125.131","http://localhost:3000");
-        corsConfig.setAllowedHeaders(allowedHeaders);
-        corsConfig.setAllowedMethods(allowedMethods);
-        corsConfig.setAllowedOrigins(allowedOrigins);
-        corsConfig.setExposedHeaders(exposedHeaders);
-        corsConfig.setMaxAge(36000L);
-        corsConfig.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-        return source;
-    }
-}
+//@Component
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//public class AjaxCorsFilter extends CorsFilter {
+////    public AjaxCorsFilter() {
+////        super(configurationSource());
+////    }
+////
+////    private static UrlBasedCorsConfigurationSource configurationSource() {
+////        CorsConfiguration corsConfig = new CorsConfiguration();
+////        List<String> allowedHeaders = Arrays.asList("*");
+////        List<String> exposedHeaders = Arrays.asList(HttpHeaders.ACCEPT,HttpHeaders.AUTHORIZATION,HttpHeaders.COOKIE,HttpHeaders.ORIGIN,HttpHeaders.HOST);
+////        List<String> allowedMethods = Arrays.asList("POST", "GET", "DELETE", "PUT", "OPTIONS");
+////        List<String> allowedOrigins = Arrays.asList("http://49.234.125.131","http://localhost:3000");
+////        corsConfig.setAllowedHeaders(allowedHeaders);
+////        corsConfig.setAllowedMethods(allowedMethods);
+////        corsConfig.setAllowedOrigins(allowedOrigins);
+////        corsConfig.setExposedHeaders(exposedHeaders);
+////        corsConfig.setMaxAge(36000L);
+////        corsConfig.setAllowCredentials(true);
+////        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+////        source.registerCorsConfiguration("/**", corsConfig);
+////        return source;
+////    }
+//}
